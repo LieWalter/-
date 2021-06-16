fd53eb1dff2e872e62df076fc7d2a3db1127aba3
LieWalter <86003991+LieWalter@users.noreply.github.com>
2021-06-16 21:56:10 +0800

    Add files via upload

---
+++ U0724008 資訊安全/傳送端/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "./src/md5.h"
+
+using namespace std;
+
+string printMD5(const string& message) {
+  return MD5(message).toStr();
+}
+
+int main()
+{
+    fstream  file;
+    char a[1000]="";
+    int m=0,Z=0,z=0;
+    srand(time(NULL));
+    for(int i=0;i<1000;i++){
+        while(true){
+            a[i]=(char)(rand()%75+48);
+            if(a[i]>57&&a[i]<65){
+                continue;
+            }
+            if(a[i]>90&&a[i]<97){
+                continue;
+            }
+            if(a[i]<=57){
+                if(m==305){
+                    continue;
+                }
+                m++;                        //產生1000亂碼
+                break;
+            }
+            if(a[i]<=90){
+                if(Z==345){
+                    continue;
+                }
+                Z++;
+                break;
+            }
+            if(z==350){
+                continue;
+            }
+            else {z++;break;}
+        }
+    }
+    string b(a),c,key;
+    b = b.substr(0,b.length()-4);
+    cout<<"key:";                           //輸入加密金鑰
+    cin>>key;
+    c=printMD5(b);                          //訊息摘要
+    c+=key;                                 //加密訊息摘要
+    file.open("1.txt", ios::out);
+    file.write(a,1000);
+    file.close();
+    file.open("2.txt",ios::out);            //產生訊息檔案與加密訊息摘要檔案
+    file<<c;
+    file.close();
+    rename("./1.txt","../接收端/1.txt");
+    rename("./2.txt","../接收端/2.txt");
+    file.open("1.txt", ios::out);
+    file.write(a,1000);
+    file.close();
+    file.open("2.txt",ios::out);
+    file<<c;
+    file.close();
+    return 0;
+}
---
+++ U0724008 資訊安全/接收端/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "./src/md5.h"
+
+using namespace std;
+
+string printMD5(const string& message) {
+  return MD5(message).toStr();
+}
+
+int main()
+{
+    fstream  file;
+    char a[1001],d[200];
+    string key;
+    file.open("1.txt",ios::in);
+    file.getline(a,1001);           //讀取訊息
+    file.close();
+    string b(a),c;
+    c=printMD5(b);                  //產生訊息摘要
+    cout<<"key:";                   //輸入解密金鑰
+    cin>>key;
+    file.open("2.txt",ios::in);
+    file.getline(d,sizeof(d));
+    file.close();
+    c+=key;                         //解密訊息摘要
+    if(c==(string)d){
+        cout<<"TRUE"<<endl;         //比對訊息摘要
+    }
+    else cout<<"false"<<endl;
+}
