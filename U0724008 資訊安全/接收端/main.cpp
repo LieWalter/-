15d50278b53b5f5f485a1b0d9ba1c0087cf34d78
LieWalter <86003991+LieWalter@users.noreply.github.com>
2021-06-17 15:24:37 +0800

    Add files via upload

--- U0724008 資訊安全/接收端/main.cpp
+++ U0724008 資訊安全/接收端/main.cpp
@@ -19,8 +19,10 @@ int main()
     file.open("1.txt",ios::in);
     file.getline(a,1001);           //讀取訊息
     file.close();
+    for(int i=0;i<1000;i++){cout<<a[i];}
     string b(a),c;
     c=printMD5(b);                  //產生訊息摘要
+    cout<<endl<<"訊息摘要:"<<c<<endl;
     cout<<"key:";                   //輸入解密金鑰
     cin>>key;
     file.open("2.txt",ios::in);
